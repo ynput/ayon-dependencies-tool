1b91a98bc4787632cbae06a9ff6656e5b5e6ea61
Petr Kalis <petr.kalis@gmail.com>
2023-05-30 17:00:57 +0200

    Removed unwanted development snippet

--- dependencies/services/__main__.py
+++ dependencies/services/__main__.py
@@ -8,19 +8,5 @@ import listener
 
 
 if __name__ == "__main__":
-    # << for development only
-    import configparser
-    toml_path = os.path.abspath("tests\\resources\\pyproject_clean.toml")
-    kwargs = {
-        # 'main_toml_path': 'C:\\Users\\petrk\\PycharmProjects\\Pype3.0\\pype\\pyproject.toml'
-        "main_toml_path": toml_path
-    }
-    config = configparser.ConfigParser()
-    config.read('../.env')
-
-    for section in config.sections():
-        for key, value in config.items(section):
-            os.environ[key] = value
-    # >>
     listener = listener.DependenciesToolListener()
     sys.exit(listener.start_listening())
