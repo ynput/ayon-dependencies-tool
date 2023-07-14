0ef3a10ca150bbc87cf4b6bbe5be9b45e806e4d5
Petr Kalis <petr.kalis@gmail.com>
2023-07-14 13:08:06 +0200

    Moved and renamed services

--- service/__main__.py
+++ service/__main__.py
@@ -8,5 +8,13 @@ import listener
 
 
 if __name__ == "__main__":
+    # << for development only
+    with open("../.env") as fp:
+        for line in fp:
+            if not line:
+                continue
+            key, value = line.split("=")
+            os.environ[key] = value.strip()
+    # >>
     listener = listener.DependenciesToolListener()
     sys.exit(listener.start_listening())
