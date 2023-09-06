1d85b0800932daffbc1098b767863814c48f306d
Petr Kalis <petr.kalis@ynput.io>
2023-09-06 12:06:40 +0200

    Fix - wrong addon name
    Frontend is unecessary

--- server/__init__.py
+++ server/__init__.py
@@ -1,17 +1,13 @@
-from typing import Any
-
 from ayon_server.addons import BaseServerAddon
 from .version import __version__
 
 from nxtools import logging
 
 
-class ShotgridAddon(BaseServerAddon):
+class DependenciesAddon(BaseServerAddon):
     name = "dependencies_tool"
     title = "Dependencies Tool"
     version = __version__
-
-    frontend_scopes: dict[str, Any] = {"settings": {}}
 
     services = {
         "Dependencies": {"image": "ynput/ayon-dependencies-tool:0.0.2"}
