f8e973235d9932626674bcea4282c4ffb49494d3
Johannes Hezer <johannes.hezer@gmail.com>
2026-06-01 11:46:08 +0200

    Update start.ps1
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- start.ps1
+++ start.ps1
@@ -172,7 +172,7 @@ function main {
     } elseif ($FunctionName -eq "create") {
         Change-Cwd
         set_env
-        & "$repo_root\.venv\Scripts\python.exe" "$($repo_root)\dependencies" create @arguments
+        & uv run python "$($repo_root)\dependencies" create @arguments
     } elseif ($FunctionName -eq "listbundles") {
         Change-Cwd
         set_env
